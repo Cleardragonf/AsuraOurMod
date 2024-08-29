9684c544f1be54d9175b53b65a69a4eb7c8f6175
chrism339@hotmail.com <chrism339@hotmail.com>
2024-08-29 08:41:25 -0500

    •  Got Entity to spawn...but they just stand there...

--- src/main/java/com/cleardragonf/ourmod/entity/custom/JackOSurpriseEntity.java
+++ src/main/java/com/cleardragonf/ourmod/entity/custom/JackOSurpriseEntity.java
@@ -1,26 +1,63 @@
 package com.cleardragonf.ourmod.entity.custom;
 
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Pose;
+import com.cleardragonf.ourmod.entity.ai.JackOSurpriseAttackGoal;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.RangedAttackGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-public class JackOSurpriseEntity extends Monster {
-    public JackOSurpriseEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
+public class JackOSurpriseEntity extends Animal {
+    public JackOSurpriseEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
+
+    private static final EntityDataAccessor<Boolean> ATTACKING =
+            SynchedEntityData.defineId(JackOSurpriseEntity.class, EntityDataSerializers.BOOLEAN);
 
     // Animation States
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
 
+    public final AnimationState attackAnimationState = new AnimationState();
+    public int attackAnimationTimout = 0;
+
+
+
     public final AnimationState spawnAnimationState = new AnimationState();  // Correct animation state for spawn
+
+    public void setAttacking(boolean attacking){
+        this.entityData.set(ATTACKING, attacking);
+    }
+
+    public boolean isAttacking(){
+        return this.entityData.get(ATTACKING);
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
+        return null;
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(ATTACKING, false);
+    }
 
     @Override
     public void tick() {
@@ -38,6 +75,18 @@ public class JackOSurpriseEntity extends Monster {
             this.idleAnimationState.start(this.tickCount);
         } else {
             --this.idleAnimationTimeout;
+        }
+
+        //WHEN
+        if (this.isAttacking() && attackAnimationTimout <= 0){
+            attackAnimationTimout = 80;
+            attackAnimationState.start(this.tickCount);
+        }else{
+            --this.attackAnimationTimout;
+        }
+
+        if(!this.isAttacking()){
+            attackAnimationState.stop();
         }
     }
 
@@ -63,7 +112,11 @@ public class JackOSurpriseEntity extends Monster {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, 2f));
+        this.goalSelector.addGoal(1, new JackOSurpriseAttackGoal(this, 10, true));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 2f));
+        this.goalSelector.addGoal(3, new RandomStrollGoal(this, 3D, 2));
+
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
